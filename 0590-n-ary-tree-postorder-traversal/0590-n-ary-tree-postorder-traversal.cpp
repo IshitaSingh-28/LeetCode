d1f6169fd23e59d45f1c5e24c9c97d7da7f3889b
Ishita Singh <96466003+IshitaSingh-28@users.noreply.github.com>
2024-08-27 02:22:45 +0530

    Time: 11 ms (78.32%), Space: 15.4 MB (13.70%) - LeetHub

---
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.cpp
@@ -0,0 +1,63 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> postorder(Node* root) {
+        vector<int> result;
+        // If the root is null, return the empty vector
+        if (root == nullptr) return result;
+
+        stack<NodeVisitPair> nodeStack;
+        nodeStack.push(NodeVisitPair(root, false));
+
+        while (!nodeStack.empty()) {
+            NodeVisitPair currentPair = nodeStack.top();
+            nodeStack.pop();
+
+            if (currentPair.isVisited) {
+                // If the node has been visited, add its value to the result
+                result.push_back(currentPair.node->val);
+            } else {
+                // Mark the current node as visited and push it back to the
+                // stack
+                currentPair.isVisited = true;
+                nodeStack.push(currentPair);
+
+                // Push all children to the stack in reverse order
+                vector<Node*>& children = currentPair.node->children;
+                for (int i = children.size() - 1; i >= 0; i--) {
+                    nodeStack.push(NodeVisitPair(children[i], false));
+                }
+            }
+        }
+
+        return result;
+    }
+
+private:
+    // Helper class to pair a node with its visited status
+    struct NodeVisitPair {
+        Node* node;
+        bool isVisited;
+
+        NodeVisitPair(Node* n, bool visited) : node(n), isVisited(visited) {}
+    };
+};
